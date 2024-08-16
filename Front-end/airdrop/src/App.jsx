ca0c539ce558299a6c7a50633aa5974dd432adec
prabh <prabh98222@gmail.com>
2024-08-17 00:17:23 +0530

    .

--- Front-end/airdrop/src/App.jsx
+++ Front-end/airdrop/src/App.jsx
@@ -5,17 +5,17 @@ import Home from './components/home/home';
 export default function App(){
   const TelegramWebApp = window?.Telegram?.WebApp;
     const [start,setStart] = useState(false)
-    const [userId, setUserId] = useState(null);
+    const [user, setUser] = useState({});
     useEffect(() => {
       if (TelegramWebApp?.initData) {
         const user = TelegramWebApp?.initDataUnsafe?.user;
          console.log("user=",user);
         if (user) {
-          setUserId(user.id); // Get the user ID
+          setUser(user); // Get the user ID
         }
       }
   
-      // Expand the web app to full screen
+
       TelegramWebApp.expand();
     }, []);
      setTimeout(()=>{
@@ -28,7 +28,8 @@ export default function App(){
         <div><img  src="/sticker.png" alt="logo" className='w-36 h-36  ' /></div>
        
  
-        <div className="w-full h-44 text-slate-50"> {userId?userId:"no user id "} </div>       </div>
+        <div className="w-full h-44 text-slate-50 m-4"> Hi , {user?user?.first_name:"no user id "} </div>   
+        <div className="w-full h-44 text-slate-50 m-4"> this is your, {user?user?.username:"no user id "} </div>        </div>
         </div>:
            
         <div className="home  bg-slate-900 w-full h-screen p-2">
