6231615c994333a48d937c3427a4fd4dd5969934
prabh <prabh98222@gmail.com>
2024-08-16 19:07:49 +0530

    .

--- Front-end/airdrop/src/App.jsx
+++ Front-end/airdrop/src/App.jsx
@@ -12,9 +12,13 @@ export default function App(){
          
   return (
      <>
-       { !start ? <div className=" bg-slate-900 w-full h-screen flex justify-center items-center ">
-        <img  src="/sticker.png" alt="logo" className='w-28 h-28 '/>
+       { !start ? <div><div className=" bg-slate-900 w-full h-screen flex justify-center ">
+        <div className='w-full h-1/2  flex items-center justify-center'><img  src="/sticker.png" alt="logo" className='w-36 h-36  ' /></div>
+       
+        </div>
+        <div className="w-full text-slate-50">PONKE </div>
         </div>:
+           
         <div className="home  bg-slate-900 w-full h-screen p-2">
             <div className="container bg-slate-900 w-full h-full">
               <Home/>
--- Front-end/airdrop/src/components/home/home.jsx
+++ Front-end/airdrop/src/components/home/home.jsx
@@ -6,9 +6,9 @@ export default function Home(){
     return (
   <>
         <div  className="logo w-full  flex justify-center items-center p-6 ">
-        <img  src="/sticker.png" alt="logo" className='w-16 h-16 '/>
+    
         </div>
-        <div className="amount_token w-full  flex justify-center items-center p-2 text-slate-50 text-4xl">
+        <div className="amount_token w-full  flex justify-center items-center p-2 text-slate-50 text-4xl  font-semibold">
           37,789
 
         </div>
