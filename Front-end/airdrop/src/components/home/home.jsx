60fa8bcd2e1538ee0e953d7b7b0cc4b3799eeda7
prabh <prabh98222@gmail.com>
2024-08-17 09:57:18 +0530

    .

--- Front-end/airdrop/src/components/home/home.jsx
+++ Front-end/airdrop/src/components/home/home.jsx
@@ -7,7 +7,7 @@ export default function Home(){
  const user = useContext(TelegramContext)
  console.log("userr=>",user);
     return (
-  <>    <div className="amount_token w-full h-8 flex justify-center items-center p-1 text-slate-50 text-xl font-serif">
+  <div className="home h-screen">     <div className="amount_token w-full h-8 flex justify-center items-center p-1 text-slate-50 text-xl font-serif">
    hi , {user?.user?.first_name}
 
 </div>
@@ -16,13 +16,13 @@ export default function Home(){
           37,789
 
         </div>
-  
+      
         <div className="footer w-full h-16 bg-slate-800 rounded-md  ">
  .
         </div>
 
 
 
-  </>
+  </div>
     )
 }
