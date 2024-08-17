d6d687996fda3a788849920aba5315b9022a81de
prabh <prabh98222@gmail.com>
2024-08-17 13:04:18 +0530

    .

--- Front-end/airdrop/src/components/home/home.jsx
+++ Front-end/airdrop/src/components/home/home.jsx
@@ -2,6 +2,7 @@ import { useContext,useState } from "react"
 import { TelegramContext } from "../../utils/store"
 export default function Home(){
     const [coins,setCoins] = useState(37739)
+    const [walletConnected ,setWalletConnected] = useState(false)
     const [tasks, setTasks] = useState([
         { id: 1, name: 'Invite 5 friends to Bubbles', description: '+30000 BUBBLES', claimed: false,coins:30000 },
         { id: 2, name: 'Share your story', description: '+1000 BUBBLES', claimed: false,coins:1000 },
@@ -19,13 +20,22 @@ export default function Home(){
         setCoins(coins+coin)
         setTasks(updatedTasks);
       };
+      const handleConnectWallet = () => {
+        // Logic to connect wallet goes here
+        setWalletConnected(true)
+      };
  const user = useContext(TelegramContext)
  console.log("userr=>",user);
     return (
   <div className="home h-screen"> 
   <div className="body p-4">
       <div className="amount_token w-full h-8 flex justify-center items-center p-1 text-slate-50 text-xl font-serif">
-   hi , {user?.user?.first_name}
+      <button
+      className={`bg-slate-200 text-xs text-slate-600 font-bold py-2 px-4 rounded-full hover:bg-slate-800 transition duration-300   ${walletConnected&& "text-slate-300"}`}
+      onClick={handleConnectWallet}
+    >
+    { walletConnected?"0x222... Connected":   "Connect Wallet"}
+    </button>
 
 </div>
      
