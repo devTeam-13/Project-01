e91557d042fd90e8a2fd379ceae2a168907bf8a1
prabh <prabh98222@gmail.com>
2024-08-17 12:32:30 +0530

    .

--- Front-end/airdrop/src/components/home/home.jsx
+++ Front-end/airdrop/src/components/home/home.jsx
@@ -1,21 +1,22 @@
 import { useContext,useState } from "react"
 import { TelegramContext } from "../../utils/store"
 export default function Home(){
-    
+    const [coins,setCoins] = useState(37739)
     const [tasks, setTasks] = useState([
-        { id: 1, name: 'Invite 5 friends to Bubbles', description: '+30000 BUBBLES', claimed: false },
-        { id: 2, name: 'Share your story', description: '+1000 BUBBLES', claimed: false },
-        { id: 3, name: 'Bonus', description: '+500 BUBBLES', claimed: false },
-        { id: 4, name: 'Join Bubbles Community group', description: '+2000 BUBBLES', claimed: false },
-        { id: 5, name: 'Make TON transaction', description: '+10000 BUBBLES', claimed: false },
+        { id: 1, name: 'Invite 5 friends to Bubbles', description: '+30000 BUBBLES', claimed: false,coins:30000 },
+        { id: 2, name: 'Share your story', description: '+1000 BUBBLES', claimed: false,coins:1000 },
+        { id: 3, name: 'Bonus', description: '+500 BUBBLES', claimed: false,coins:500 },
+        { id: 4, name: 'Join Bubbles Community group', description: '+2000 BUBBLES', claimed: false,coins:2000 },
+        { id: 5, name: 'Make TON transaction', description: '+10000 BUBBLES', claimed: false,coins:10000 },
       ]);
-      const claimTask = (id) => {
+      const claimTask = (id,coin) => {
         const updatedTasks = tasks.map((task) => {
           if (task.id === id) {
             return { ...task, claimed: true };
           }
           return task;
         });
+        setCoins(coins+coin)
         setTasks(updatedTasks);
       };
  const user = useContext(TelegramContext)
@@ -29,7 +30,7 @@ export default function Home(){
 </div>
      
         <div className="amount_token w-full h-40 flex justify-center items-center p-2  text-slate-50 text-4xl  font-bold">
-          37,789
+          {coins.toLocaleString()}
 
         </div>
         <div className="TaskSection w-full  ">
@@ -48,7 +49,7 @@ export default function Home(){
               className={`px-3 py-2 rounded-full ${
                 task.claimed ? ' cursor-not-allowed text-sm' : 'bg-gray-800 '
               } text-slate-200`}
-              onClick={() => claimTask(task.id)}
+              onClick={() => claimTask(task.id,task.coins)}
               disabled={task.claimed}
             >
               {task.claimed ? 'Claimed' : 'Claim'}
