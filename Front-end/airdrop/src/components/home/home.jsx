58f52bdadea269d6b716776c49761eaa6a4592f9
prabh <prabh98222@gmail.com>
2024-08-17 11:36:20 +0530

    .

--- Front-end/airdrop/src/components/home/home.jsx
+++ Front-end/airdrop/src/components/home/home.jsx
@@ -14,18 +14,22 @@ export default function Home(){
 
 </div>
      
-        <div className="amount_token w-full h-40 flex justify-center items-center p-2 text-slate-50 text-4xl  font-bold">
+        <div className="amount_token w-full h-40 flex justify-center items-center p-2  text-slate-50 text-4xl  font-bold">
           37,789
 
+        </div>
+        <div className="tasks bg-slate-400 w-full ">
+              d
+           
         </div>
         </div>
       
         <div className="footer w-full h-16 bg-slate-800  flex rounded-full  ">
 
-            <div className="menu w-1/4 flex justify-center items-center   text-slate-400">a</div>
-            <div className="menu w-1/4 flex justify-center items-center  text-slate-400">b</div>
-            <div className="menu  w-1/4 flex justify-center items-center  text-slate-400">c</div>           
-            <div className="menu  w-1/4 flex justify-center items-center  text-slate-400">c</div>
+            <div className="menu w-1/4 flex justify-center items-center   text-slate-400">H</div>
+            <div className="menu w-1/4 flex justify-center items-center  text-slate-400">L</div>
+            <div className="menu  w-1/4 flex justify-center items-center  text-slate-400">T</div>           
+            <div className="menu  w-1/4 flex justify-center items-center  text-slate-400">I</div>
 
         </div>
 
